ea9eac4be57834d8bf3830a1a2afd58caba230d1
ameureka <166780113+ameureka@users.noreply.github.com>
2024-05-31 03:50:00 +0800

    45646

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+// // 添加portfilio 标签
 // const navItems = {
 //   '/': {
 //     name: 'home',
@@ -10,12 +11,20 @@ import Link from 'next/link';
 //   '/blog': {
 //     name: 'blog',
 //   },
+//   '/portfolio': {
+//     name: 'portfolio',
+//   },
 //   '/guestbook': {
 //     name: 'guestbook',
 //   },
 // };
 
-// 添加portfilio 标签
+
+// 通过在 navItems 对象中为 '/portfolio' 项目设置一个完整的 URL 来实现这一目标。
+// 为了确保其他导航项仍然使用相对路径，你可以在 Navbar 组件中添加逻辑来处理这种情况。
+
+
+// 添加portfolio 标签并设置外部链接
 const navItems = {
   '/': {
     name: 'home',
@@ -28,12 +37,13 @@ const navItems = {
   },
   '/portfolio': {
     name: 'portfolio',
+    external: true,
+    url: 'https://portfoliopage.ameureka.com',
   },
   '/guestbook': {
     name: 'guestbook',
   },
 };
-
 
 
 export function Navbar() {
@@ -45,7 +55,22 @@ export function Navbar() {
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
+            {Object.entries(navItems).map(([path, { name, external, url }]) => {
+              // 如果是外部链接，使用 <a> 标签
+              if (external) {
+                return (
+                  <a
+                    key={path}
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
+                  >
+                    {name}
+                  </a>
+                );
+              }
+              // 否则使用 <Link> 组件
               return (
                 <Link
                   key={path}
