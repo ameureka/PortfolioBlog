d757553a2ea6c2ca25a8681a494c6605662ee514
ameureka <166780113+ameureka@users.noreply.github.com>
2024-08-11 09:40:35 +0800

    furongqi

--- app/page.tsx
+++ app/page.tsx
@@ -399,9 +399,7 @@ export default function Page() {
             </svg>
           </a>
         </div>
-      </div>
 
-      <div className="my-8 flex h-14 w-full flex-row space-x-2 overflow-x-auto">
         {/* 电子书链接001 人人都会用AIGC */}
         <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
           <a href="https://aigcbook.ameureka.com">
