84a2a4679b0faa17cac4b615f6a51fd6ed7a26ac
ameureka <166780113+ameureka@users.noreply.github.com>
2025-07-08 23:40:30 +0800

    delete youtube

--- app/page.tsx
+++ app/page.tsx
@@ -47,47 +47,49 @@ function ArrowIcon() {
   );
 }
 
-function ChannelLink({ img, link, name }) {
-  return (
-    <div className="group flex w-full">
-      <a
-        href={link}
-        target="_blank"
-        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
-      >
-        <div className="flex items-center space-x-3">
-          <div className="relative h-16">
-            <Image
-              alt={name}
-              src={img}
-              height={64}
-              width={64}
-              sizes="33vw"
-              className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
-              priority
-            />
-            <div className="relative -right-10 -top-6 inline-flex h-6 w-6 items-center rounded-full border border-neutral-200 bg-white p-1 dark:border-neutral-700">
-              <svg width="15" height="11" role="img" aria-label="YouTube logo">
-                <use href="/sprite.svg#youtube" />
-              </svg>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <p className="font-medium text-neutral-900 dark:text-neutral-100">
-              {name}
-            </p>
-            <Suspense fallback={<p className="h-6" />}>
-              <Subs name={name} />
-            </Suspense>
-          </div>
-        </div>
-        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
-          <ArrowIcon />
-        </div>
-      </a>
-    </div>
-  );
-}
+// youtube订阅页面展示部分
+
+// function ChannelLink({ img, link, name }) {
+//   return (
+//     <div className="group flex w-full">
+//       <a
+//         href={link}
+//         target="_blank"
+//         className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+//       >
+//         <div className="flex items-center space-x-3">
+//           <div className="relative h-16">
+//             <Image
+//               alt={name}
+//               src={img}
+//               height={64}
+//               width={64}
+//               sizes="33vw"
+//               className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
+//               priority
+//             />
+//             <div className="relative -right-10 -top-6 inline-flex h-6 w-6 items-center rounded-full border border-neutral-200 bg-white p-1 dark:border-neutral-700">
+//               <svg width="15" height="11" role="img" aria-label="YouTube logo">
+//                 <use href="/sprite.svg#youtube" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="flex flex-col">
+//             <p className="font-medium text-neutral-900 dark:text-neutral-100">
+//               {name}
+//             </p>
+//             <Suspense fallback={<p className="h-6" />}>
+//               <Subs name={name} />
+//             </Suspense>
+//           </div>
+//         </div>
+//         <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+//           <ArrowIcon />
+//         </div>
+//       </a>
+//     </div>
+//   );
+// }
 // // youtube订阅获取接口信息
 // async function Subs({ name }: { name: string }) {
 //   noStore();
